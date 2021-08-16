4131584d6ad51ad17828129a9a3d86c66958a0b0
BogdanKotarlic <31574433+BogdanKotarlic@users.noreply.github.com>
2021-08-16 13:36:20 +0200

    new upload
    upload classes section

--- scripts/script.js
+++ scripts/script.js
@@ -17,4 +17,28 @@ function changeSlide(direction) {
     }
     currentImg.removeClass('active');
 }
-  
+
+var tabButtons = document.querySelectorAll(".buttonContainer button")
+var tabPanels = document.querySelectorAll(".tabContainer .tab")
+
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.borderTop = "none";
+    node.style.color = "";
+    node.style.fontWeight = "normal"
+  });
+  tabButtons[panelIndex].style.backgroundColor = "#ffffff";
+  tabButtons[panelIndex].style.color = "#002f9d";
+  tabButtons[panelIndex].style.borderTop = "5px solid #38bdff";
+  tabButtons[panelIndex].style.fontWeight = "bold"
+
+  tabPanels.forEach(function (node) {
+    node.style.display = "none";
+  });
+
+  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.backgroundColor = "colorCode";
+
+}
+showPanel(0);
