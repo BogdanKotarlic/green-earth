85dd57f4ad8102d4a9863a42c2fd7bbd03fca1cb
BogdanKotarlic <31574433+BogdanKotarlic@users.noreply.github.com>
2021-08-18 09:14:11 +0200

    upload

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ Single responsive page Green Earth.
 1. npm-init
 2. npm install --global gulp-cli
 3. npm install --save-dev gulp gulp-sass sass gulp-autoprefixer gulp-clean-css gulp-terser
-
-# Run
-Please open this page with live server.
--- scripts/script-json.js
+++ scripts/script-json.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
     // Because of CORS policy works only with live server
-    var db = [];
-    var json = (function () {
-        var json = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': './json/db.json',
-            'dataType': 'json',
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })
-    db = json();
+    // var db = [];
+    // var json = (function () {
+    //     var json = null;
+    //     $.ajax({
+    //         'async': false,
+    //         'global': false,
+    //         'url': './json/db.json',
+    //         'dataType': 'json',
+    //         'success': function (data) {
+    //             json = data;
+    //         }
+    //     });
+    //     return json;
+    // })
+    // db = json();
 
-    // VERSION WITHOUT LIVE SERVER
-    // var db = [
-    //     {
-    //         "name": "stock1",
-    //         "path": "./images/stock1.jpg",
-    //         "description": "stock1 img"
-    //     },
-    //     {
-    //         "name": "stock2",
-    //         "path": "./images/stock2.jpg",
-    //         "description": "stock2 img"
-    //     },
-    //     {
-    //         "name": "stock3",
-    //         "path": "./images/stock3.jpg",
-    //         "description": "stock3 img"
-    //     }
-    // ]
+    //VERSION WITHOUT LIVE SERVER
+    var db = [
+        {
+            "name": "stock1",
+            "path": "./images/stock1.jpg",
+            "description": "stock1 img"
+        },
+        {
+            "name": "stock2",
+            "path": "./images/stock2.jpg",
+            "description": "stock2 img"
+        },
+        {
+            "name": "stock3",
+            "path": "./images/stock3.jpg",
+            "description": "stock3 img"
+        }
+    ]
 
     $.each(db, function (index, value) {
         $(".slider").append("<img src='"+value.path+"' alt='"+value.description+"'/>")
